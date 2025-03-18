25f26e1c2351a6f66bf2bd0a346821e02a88cc77
snippetarnab <arnabvivo2020@gmail.com>
2025-03-18 11:04:36 +0530

    update the env

---
+++ frontend/node_modules/chat-app/frontend/node_modules/chat-app/frontend/node_modules/chat-app/frontend/node_modules/chat-app/backend/src/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import path from "path";
+
+dotenv.config();
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+  });
